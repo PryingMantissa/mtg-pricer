fd621bdd59e525231417925bc56da19849c2f421
Gleesin <seriouz.accz@gmail.com>
2014-07-27 21:05:10 +0200

    Removed unused icons

--- src/main/java/sk/lkce/mtgp/gui/ResourceLoader.java
+++ src/main/java/sk/lkce/mtgp/gui/ResourceLoader.java
@@ -13,16 +13,16 @@ public class ResourceLoader {
 	private static int ICON_HEIGHT = 20;
 	private static int ICON_WIDTH = 20;
 	public static final ImageIcon ICON_ADD = loadIcon("/icons/file_add.png",ICON_WIDTH,ICON_HEIGHT);
-	public static final ImageIcon ICON_REMOVE = loadIcon("/icons/file_delete2.png",ICON_WIDTH,ICON_HEIGHT);
+	public static final ImageIcon ICON_REMOVE = loadIcon("/icons/file_delete.png",ICON_WIDTH,ICON_HEIGHT);
 	//Slightly thinner as its not symmetric.
-	public static final ImageIcon ICON_IMPORT = loadIcon("/icons/import1.png",ICON_WIDTH-1,ICON_HEIGHT);
+	public static final ImageIcon ICON_IMPORT = loadIcon("/icons/import.png",ICON_WIDTH-1,ICON_HEIGHT);
 	public static final ImageIcon ICON_GO = loadIcon("/icons/play.png",ICON_WIDTH,ICON_HEIGHT);
-	public static final ImageIcon ICON_STOP = loadIcon("/icons/stop-icon_40.png",ICON_WIDTH,ICON_HEIGHT);
+	public static final ImageIcon ICON_STOP = loadIcon("/icons/stop.png",ICON_WIDTH,ICON_HEIGHT);
 	public static final ImageIcon ICON_BROWSER = loadIcon("/icons/browser.png",ICON_WIDTH,ICON_HEIGHT);
 	public static final ImageIcon ICON_APP = loadIcon("/icons/app_icon.png",ICON_WIDTH,ICON_HEIGHT);
 	public static final ImageIcon ICON_LOADING = loadIcon("/icons/loading.gif");
 	public static final ImageIcon ICON_STOPPING = loadIcon("/icons/stopping.gif");
-	public static final ImageIcon ICON_SEARCH_FINISHED = loadIcon("/icons/tick_1.png",10,10);
+	public static final ImageIcon ICON_SEARCH_FINISHED = loadIcon("/icons/tick.png",10,10);
 	public static final ImageIcon ICON_SEARCH_SETTINGS = loadIcon("/icons/panel_settings.png",12,12);
 	
 	/**
